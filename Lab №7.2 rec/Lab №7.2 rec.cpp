2c25e07e60a5cd2887574ff33a587af7d51a8f8e
Volodya <chuchman002@gmail.com>
2023-11-10 01:03:18 +0200

    Add project files.

---
+++ Lab №7.2 rec/Lab №7.2 rec.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <ctime>
+
+void generateMatrix(int matrix[][5], int n, int i = 0, int j = 0) {
+    if (i < n) {
+        if (j < n) {
+            matrix[i][j] = rand() % 100; 
+            generateMatrix(matrix, n, i, j + 1);
+        }
+        else {
+            generateMatrix(matrix, n, i + 1, 0);
+        }
+    }
+}
+
+void printMatrix(int matrix[][5], int n, int i = 0, int j = 0) {
+    if (i < n) {
+        if (j < n) {
+            std::cout << matrix[i][j] << "\t";
+            printMatrix(matrix, n, i, j + 1);
+        }
+        else {
+            std::cout << "\n";
+            printMatrix(matrix, n, i + 1, 0);
+        }
+    }
+}
+
+int findMaxInMainDiagonal(int matrix[][5], int n, int i = 0, int maxElem = -1) {
+    if (i < n) {
+        if (matrix[i][i] > maxElem) {
+            maxElem = matrix[i][i];
+        }
+        return findMaxInMainDiagonal(matrix, n, i + 1, maxElem);
+    }
+    else {
+        return maxElem;
+    }
+}
+
+int main() {
+    srand(time(0)); 
+    int n = 5; 
+    int matrix[5][5];
+    generateMatrix(matrix, n);
+    std::cout << "matrix:\n";
+    printMatrix(matrix, n);
+    int maxElem = findMaxInMainDiagonal(matrix, n);
+    std::cout << "max element diagonaly: " << maxElem << "\n";
+    return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,28 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab №7.2 rec/Lab №7.2 rec.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(TestGenerateMatrix)
+		{
+			const int n = 5;
+			int matrix[n][n];
+
+			generateMatrix(matrix, n);
+
+			// Перевірка, чи всі елементи матриці знаходяться в правильному діапазоні
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++) {
+					Assert::IsTrue(matrix[i][j] >= 0 && matrix[i][j] < 100);
+				}
+			}
+		}
+	};
+}
